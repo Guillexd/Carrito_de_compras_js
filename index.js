330c76bb448116cd13cb7e3de24c019f9f7542e2
GuillexD <enrique2003junior@gmail.com>
2022-08-20 22:10:44 -0500

    Agregando

--- index.js
+++ index.js
@@ -71,8 +71,10 @@ function cargandoEvents() {
     //evento para poder vaciar el carrito de compras desde que se inicie o actualice la página
     d.querySelector("#vaciarCarrito").addEventListener("click", vaciarCarrito);
     //a cada <span> le agrego un boton con un evento para aumentar o dismunuir un producto
-    $aside.addEventListener("click", aumentarProducto);
-    $aside.addEventListener("click", disminuirProducto);
+    // $aside.addEventListener("click", aumentarProducto);
+    // $aside.addEventListener("click", disminuirProducto);
+    d.addEventListener("click", aumentarProducto);
+    d.addEventListener("click", disminuirProducto);
     //evento para mostra el buscador
     d.addEventListener("click", buscadorProd);
     //evento filtrador del buscador
@@ -95,7 +97,7 @@ d.addEventListener("DOMContentLoaded", () => {
     //funcion para cargar los eventos iniciales
     cargandoEvents();
     //convierto el localStorage a un array de objetos
-    let array = JSON.parse(localStorage.getItem("Carrito"));
+    const array = JSON.parse(localStorage.getItem("Carrito"));
     //pongo en el carrito la cantidad de productos agregados
     $contadorDelCArrito.innerHTML = array.length;
     //primer div del aside
@@ -585,15 +587,9 @@ function filtrador(e){
             </div>
         `;
     }
-
-    // if (e.key === "Backspace"){
-    //     d.querySelectorAll(".productos").forEach(prod => {
-    //         prod.firstElementChild.getAttribute("alt").toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()) ? prod.classList.remove("d-none") : null
-    //     });
-    // };
-
 };
 
+//función para elegir el producto filtrado
 const buscarFiltro = e => {
     d.querySelectorAll(".productos").forEach(prod => {
         prod.firstElementChild.getAttribute("alt").toLocaleLowerCase().includes(e.target.textContent.toLocaleLowerCase()) ? prod.classList.remove("d-none") : prod.classList.add("d-none")
@@ -609,3 +605,195 @@ const buscarFiltro = e => {
         </div>
     `;
 }
+
+            // carrito.html
+//variables
+const $formulario = d.getElementById("formulario"),
+$inputs = d.querySelectorAll(".formularioInput"),
+expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^(\+?[\d-_\s]{2,4})?\d{7,14}$/ // 7 a 14 numeros.
+},
+campos = {
+	nombre: false,
+	correo: false,
+	telefono: false
+};
+
+//evento para enviar formulario
+$formulario.addEventListener("submit", (e) => {
+
+	e.preventDefault();
+
+	const $terminos = d.getElementById("terminos");
+	if(campos.nombre && campos.correo && campos.telefono && $terminos.checked ){
+        //sweet alert 2
+        Swal.fire({
+            title: '¡Advertencia!',
+            text: 'El formulario se enviará, ¿estás seguro?',
+            icon: 'info',
+            showConfirmButton: true,
+            confirmButtonText: 'Confirmar',
+            showCancelButton: true,
+            cancelButtonText: 'Denegar',
+            allowOutsideClick: false,
+            customClass: {
+                popup: "contenedor"
+            }
+        }).then(resolve => {
+            if (resolve.isConfirmed){
+
+                $formulario.reset();
+
+                d.getElementById("formularioMensaje").classList.add("opacity-0");
+                d.getElementById("formularioMensaje").classList.remove("opacity-100");
+                d.getElementById("formularioExito").classList.add("opacity-100");
+                setTimeout(() => {
+                    d.getElementById("formularioExito").classList.remove("opacity-100");
+                }, 7000);
+
+                d.querySelectorAll(".icon").forEach(icon => {
+                    icon.classList.add('fa-times-circle');
+                    icon.classList.remove('fa-check-circle');
+                    icon.classList.remove("opacity-100");
+                    icon.classList.add("opacity-0");
+                });
+                //sweet alert 2
+                Swal.fire({
+                    title: '¡Envio exitoso!',
+                    text: 'El formulario fue enviado',
+                    icon: 'success',
+                    showConfirmButton: false,
+                    timer: 1000,
+                    timerProgressBar: true,
+                    customClass: {
+                        popup: "contenedor"
+                    }
+                });
+            } else{
+                //sweet alert 2
+                Swal.fire({
+                    title: 'Cancelado',
+                    icon: 'error',
+                    showConfirmButton: false,
+                    timer: 1000,
+                    timerProgressBar: true,
+                    customClass: {
+                        popup: "contenedor"
+                    }
+                });
+            };
+        });
+    } else{
+        d.querySelector("#formularioMensaje").classList.add("opacity-100")
+        setTimeout(()=>{
+            d.querySelector("#formularioMensaje").classList.remove("opacity-100");
+        }, 3000)
+
+        //sweet alert 2
+        Swal.fire({
+            title: 'No se puede enviar el formulario',
+            icon: 'error',
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            customClass: {
+                popup: "contenedor"
+            }
+        });
+    }
+});
+
+//función para validar inputs parte 1
+const validarFormulario = (e) => {
+    e.target.innerHTML="holaxdxd"
+	switch (e.target.id) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, e.target.id);
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, e.target.id);
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, e.target.id);
+		break;
+	};
+};
+
+//funcion para validar inputs parte 2
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+        console.log("funciona")
+		d.getElementById(`texto-${campo}`).classList.remove("opacity-100");
+        d.getElementById(`texto-${campo}`).classList.add("opacity-0");
+		d.querySelector(`#icon-${campo}`).classList.add("fa-check-circle");
+		d.querySelector(`#icon-${campo}`).classList.remove("fa-times-circle");
+        d.querySelector(`#icon-${campo}`).classList.add("opacity-100");
+        d.querySelector(`#icon-${campo}`).classList.remove("opacity-0");
+		campos[campo] = true;
+	} else {
+        console.log("jaja no");
+		d.getElementById(`texto-${campo}`).classList.remove("opacity-0");
+        d.getElementById(`texto-${campo}`).classList.add("opacity-100");
+		d.querySelector(`#icon-${campo}`).classList.add('fa-times-circle');
+		d.querySelector(`#icon-${campo}`).classList.remove('fa-check-circle');
+        d.querySelector(`#icon-${campo}`).classList.add("opacity-100");
+        d.querySelector(`#icon-${campo}`).classList.remove("opacity-0");
+		campos[campo] = false;
+	};
+};
+
+//agregando los eventos a los inputs
+$inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+// (()=>{
+
+//     document.querySelector("#correo").value="Holaxdxdx"
+// })()
+
+// (()=>{
+//     //funcion para cargar los eventos iniciales
+//     // cargandoEvents();
+//     //convierto el localStorage a un array de objetos
+//     const array = JSON.parse(localStorage.getItem("Carrito"));
+//     //primer div del aside
+//     const $prodFormulario = d.querySelector("#productosLocalStorage")
+//     //recorremos el localStorage e insertamos los productos en el carrito
+//     array.forEach((obj) => {
+//         const $section = d.createElement("section");
+//         $section.classList.add("cards");
+//         $section.classList.add("d-flex");
+//         $section.classList.add("justify-content-evenly");
+//         $section.innerHTML = `
+//                 <div class="divImagen">
+//                     <img src="../${obj.img}" alt="${obj.nombre}">
+//                 </div>
+
+//                 <div class="divDescrip text-center">
+//                     <span>${obj.nombre}</span>
+//                     <span id="precio">$${obj.precio}</span>
+//                     <div id="${obj.id}">
+//                         <i id="menosSecond" class="fa-solid fa-minus"></i>
+//                         <span id="contador">${obj.cantidad}</span>
+//                         <i id="masSecond" class="fa-solid fa-plus"></i>
+//                     </div>
+//                 </div>
+
+//                 <i id="trash${obj.id}" class="fa-solid fa-trash notification-alert"></i>
+//                 `;
+
+//         $fragment.prepend($section);
+
+//         //a cada section le agrego un boton con un evento para quitar un producto
+//         $section.querySelector(`#trash${obj.id}`).addEventListener('click', quitarProducto);
+//     });
+
+//     //agregamos el fragment al aside
+//     $prodFormulario.prepend($fragment)//pone de primer hijo 
+
+//     //llamo una función 
+//     actualizarCarrito();
+// })();
